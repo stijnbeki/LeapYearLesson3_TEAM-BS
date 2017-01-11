fe630fbec2b5ad223b5f34dc94471e3f80ad4fca
Middelburg <bart.middelburg@wur.nl>
2017-01-11 15:32:11 +0100

    Test phase - will soon be finished.

---
+++ Functions/LeapYearAdvanced.R
@@ -0,0 +1,24 @@
+is.leapAD=function(year){
+  
+  if (!is.numeric(year)) 
+  {
+    warning('argument of class numeric expected')
+  } 
+  
+  else
+  {
+    if (year < 1582) 
+    {
+      sprintf(paste(year, 'is out of the valid range')) 
+    } 
+    
+    else{
+      {
+        return(((year %% 4 == 0) & (year %% 100 != 0)) | (year %% 400 == 0))
+      }
+    }}
+}
+
+
+
+
