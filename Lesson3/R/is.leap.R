f058a1a14f6f116e6509098eeca70bb593829ff2
Ubuntu User <ubuntu@packer-ubuntu-16.04-server-one>
2017-01-12 16:48:46 +0100

    Changed the layout of everything. Now per lesson.
    gr Stijn

---
+++ Lesson3/R/is.leap.R
@@ -0,0 +1,57 @@
+###Team BS###
+#11-01-2017#
+###############################################################################SCRIPT_1#
+
+###name of the leap year function:
+
+is.leap <- function(x)
+
+  ### 1- check if the class is numeric###
+  { 
+  
+  if (!is.numeric(x)) {
+    leap <- warning('argument of class numeric expected')
+    } 
+
+    else
+      {
+      ### 2- Check if it is not before 1582
+      if (x < 1582) 
+          {
+          leap <- sprintf(paste(x, 'is out of the valid range')) 
+          } 
+        
+        ### 3- check if it can be divided by 4, if NOT ---> FALSE
+        else
+              { 
+              if((x/4)%%1 != 0)
+              {
+              leap <- FALSE
+              }
+          
+          ### 4&5- check if: it can be divided by 100, and can not be divided by 400 --> FALSE      
+          else
+                  {
+                  if ((x/100)%%1 == 0 & (x/400)%%1 != 0)
+                  {
+                  leap <- FALSE
+                  }
+                    
+                    ### The rest ---> TRUE
+                    else 
+                        {
+                        leap <- TRUE
+                        }
+        
+      }
+    } }
+  
+  return(leap)
+  
+  }
+
+
+
+
+
+
