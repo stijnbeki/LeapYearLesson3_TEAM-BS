941b4a286d526672607da029db8f2938b8431842
Ubuntu User <ubuntu@packer-ubuntu-16.04-server-one>
2017-01-26 18:01:41 +0100

    Lesson 14 Team BS

---
+++ Lesson_14/Assignement_Les14.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 26 09:20:51 2017
+
+@author: ubuntu
+"""
+
+#team BS#
+#Bart Middelburg#
+#Stijn Beernink#
+#Thu 26 Jan######
+
+import pip
+from twython import Twython
+import pandas as pd 
+import json
+import datetime 
+#Geocoder can be installed in terminal by: sudo pip install geocoder
+import geocoder
+
+
+##codes to access twitter API. 
+APP_KEY = 'N4omboT3gmZb8dqhzKizormPr'
+APP_SECRET = 'xfF7o8pvqvK1zx8kxRX6uFNI9YNO3lSOYzY8e8PDyQqB4eyhZi'
+OAUTH_TOKEN = '824524662394814465-xeylDNzPxXAJWgOUitffDiPL2oFKvcY'
+OAUTH_TOKEN_SECRET = '5GjzQUf9PtmmWHVPJUd5Ae2utcFWiVjhywnXF2n6A4OiR'
+
+##initiating Twython object 
+twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+
+search_results = twitter.search(q='ongeval',count=2000)
+
+outcome_coord = []
+text = []
+    
+#parsing out 
+for tweet in search_results["statuses"]:
+    if tweet['place'] != None:
+        tweettext = tweet['text']
+        full_place_name = tweet['place']['full_name']
+        g = geocoder.google(full_place_name)
+        outcome_coord.append(g.latlng)
+        text.append(str(tweettext))
+    
+    print '==========================='
+
+df_plot= pd.Dataframe({'Coordinates': outcome_coord,
+                      'Tweet': text})
+
+import folium
+mapit = folium.Map( location=[52.2196884, 5.0936663], zoom_start=8 )
+for coord in outcome_coord:
+    folium.Marker( location=[ coord[0], coord[1] ]).add_to( mapit )
+
+mapit.save('map.html')
+
+for i in range(0,len(outcome_coord)):
+    folium.Marker( location=[ outcome_coord[i][0], outcome_coord[i][1] ]).add_to( mapit )
+
+mapit.save('map.html')
+
